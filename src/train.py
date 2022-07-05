7e88ccb982419ae9808e43e947a52a1a7fe9758d
mithun-tm <108567882+mithun-tm@users.noreply.github.com>
2022-07-06 03:49:58 +0530

    updated train params
    n_estimators changed from 5 to 10

--- src/train.py
+++ src/train.py
@@ -13,7 +13,7 @@ df = pd.DataFrame(data=iris.data, columns = ['sepal_length', 'sepal_width', 'pet
 df['label'] = iris.target
 df = df [:120]
 
-RF_model = RandomForestClassifier(n_estimators=5, max_depth = 5)
+RF_model = RandomForestClassifier(n_estimators=10, max_depth = 5)
 RF_model.fit(df[['sepal_length', 'sepal_width', 'petal_length', 'peta_width']], df['label'])
 y_pred = RF_model.predict(df[['sepal_length', 'sepal_width', 'petal_length', 'peta_width']])
 acc = accuracy_score(df['label'], y_pred)
