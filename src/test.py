fa9ab66128ad3b88142b1dd45743b51db3de30db
mithun.tm <mithun.tm@swiggy.in>
2022-07-05 23:58:41 +0530

    Training Verified

--- src/test.py
+++ src/test.py
@@ -3,13 +3,15 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-filename = "../models/saved_model"
+filename = "models/saved_model"
 
-df = pd.read_csv("../data/test_data.csv"")
+df = pd.read_csv("data/test_data.csv")
 
 
 RF_model = pickle.load(open(filename, 'rb'))
 y_pred = RF_model.predict(df[['sepal_length', 'sepal_width', 'petal_length', 'peta_width']])
-accuracy_score(df['label'], y_pred)
+acc = accuracy_score(df['label'], y_pred)
+
+print("Accuracy : ",acc)
 
 
--- src/train.py
+++ src/train.py
@@ -3,16 +3,16 @@ import pickle
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score
 
-filename = "../models/saved_model"
+filename = "models/saved_model"
 
-df = pd.read_csv("../data/train_data.csv")
+df = pd.read_csv("data/train_data.csv")
 
 
-RF_model = RandomForestClassifier()
+RF_model = RandomForestClassifier(n_estimators=5, max_depth = 3)
 RF_model.fit(df[['sepal_length', 'sepal_width', 'petal_length', 'peta_width']], df['label'])
 y_pred = RF_model.predict(df[['sepal_length', 'sepal_width', 'petal_length', 'peta_width']])
-accuracy_score(df['label'], y_pred)
+acc = accuracy_score(df['label'], y_pred)
 
-filename = "RF_model"
+print("Accuracy : ", acc)
 pickle.dump(RF_model, open(filename, 'wb'))
 
