1b20806710602ac8126095f4e2745372e6e58ec0
mithun-tm <108567882+mithun-tm@users.noreply.github.com>
2022-07-06 01:39:58 +0530

    Update train.py

--- src/train.py
+++ src/train.py
@@ -14,7 +14,7 @@ df = df [:120]
 
 
 
-RF_model = RandomForestClassifier(n_estimators=5, max_depth = 3)
+RF_model = RandomForestClassifier(n_estimators=4, max_depth = 3)
 RF_model.fit(df[['sepal_length', 'sepal_width', 'petal_length', 'peta_width']], df['label'])
 y_pred = RF_model.predict(df[['sepal_length', 'sepal_width', 'petal_length', 'peta_width']])
 acc = accuracy_score(df['label'], y_pred)
