e190e51e3490100a5256ef3986d0aeae72cc1213
mithun.tm <mithun.tm@swiggy.in>
2022-07-05 21:33:01 +0530

    First Commit from local

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='CI Steps for ML',
+    author='Mithun T M',
+    license='',
+)
